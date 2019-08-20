3402059d427e3bba31217662455cd44d8233051d
Hans gpp <hans_selmo@hotmail.com>
2019-08-21 00:09:19 +0200

    checkboxes

--- hans_visual/src/controls.hpp
+++ hans_visual/src/controls.hpp
@@ -13,45 +13,45 @@ class controls;
 extern controls camera;
 
 class controls {
-    // Time
-    float lastTime = 0.0;
-    float currentTime;
-    float deltaTime;
+	// Time
+	float lastTime = 0.0;
+	float currentTime;
+	float deltaTime;
 
-    // Mouse
-    double xpos, ypos;
-    double xpos0, ypos0;
-    bool L_pressed, R_pressed;
+	// Mouse
+	double xpos, ypos;
+	double xpos0, ypos0;
+	bool L_pressed, R_pressed;
 
-    // camera parameters
-    glm::vec3 position;
-    float horizontalAngle;
-    float verticalAngle;
-    float initialFoV;
-    float speed;
-    float mouseSpeed;
-    glm::vec3 sphere_center;
-    float radius;
-    float scroll_speed;
-    float minimum_radius;
+	// camera parameters
+	glm::vec3 position;
+	float horizontalAngle;
+	float verticalAngle;
+	float initialFoV;
+	float speed;
+	float mouseSpeed;
+	glm::vec3 sphere_center;
+	float radius;
+	float scroll_speed;
+	float minimum_radius;
 
-    int camera_mode;
-    glm::mat4 ViewMatrix;
-    glm::mat4 ProjectionMatrix;
+	int camera_mode;
+	glm::mat4 ViewMatrix;
+	glm::mat4 ProjectionMatrix;
 
-    void computeMatricesFromInputs_FPS(GLFWwindow*);
-    void computeMatricesFromInputs_spherical(GLFWwindow*);
+	void computeMatricesFromInputs_FPS(GLFWwindow*);
+	void computeMatricesFromInputs_spherical(GLFWwindow*);
 
 public:
-    controls(int mode = FPS);
-    void adjustments(GLFWwindow *window);
-    void computeMatricesFromInputs(GLFWwindow* window);
+	controls(int mode = FPS);
+	void adjustments(GLFWwindow *window);
+	void computeMatricesFromInputs(GLFWwindow* window);
 
-    glm::mat4 getViewMatrix();
-    glm::mat4 getProjectionMatrix();
+	glm::mat4 getViewMatrix();
+	glm::mat4 getProjectionMatrix();
 
-    friend void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
-    friend void scrollCallback(GLFWwindow *window, double xOffset, double yOffset);
+	friend void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
+	friend void scrollCallback(GLFWwindow *window, double xOffset, double yOffset);
 };
 
 // Callback functions: Get mouse click - Get mouse out/in window - Get cursor position
